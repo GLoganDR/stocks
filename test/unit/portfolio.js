74f407caa1c2122b16fffaac5daed0f89a1eb453
Brian Hiatt <brian.hiatt41@gmail.com>
2014-07-18 04:25:19 -0500

    added stock array

--- test/unit/portfolio.js
+++ test/unit/portfolio.js
@@ -1,11 +1,14 @@
-'use strict';
+/* jshint expr:true */
+/* global describe, it */
 
+'use strict';
 
 var expect = require('chai').expect;
 var Portfolio = require('../../app/models/portfolio');
+var Stock = require('../../app/models/stock');
 
 describe('Portfolio', function (){
-  describe ('aggressive', function(){
+  describe('constructor', function(){
     it('should create a new portfolio of aggressive tech stocks', function(){
       var tech = new Portfolio('Aggressive Portfolio');
 
@@ -14,5 +17,20 @@ describe('Portfolio', function (){
       expect(tech.stocks).to.have.length(0);
     });
   });
-  
+  describe('#add', function(){
+    it('should add to the array of stocks to portfolio and add to existing stocks', function(){
+      var tech = new Portfolio('Aggressive Portfolio');
+      var aapl = new Stock('AAPL', 50);
+      var amzn = new Stock('AMZN', 30);
+      var aapl2 = new Stock('AAPL', 25);
+      tech.add(aapl);
+      tech.add(amzn);
+      tech.add(aapl2);
+
+      expect(tech.stocks).to.have.length(2);
+      expect(tech.stocks[0]).to.be.instanceof(Stock);
+      expect(tech.stocks[0].count).to.equal(75);
+      expect(tech.stocks[1].count).to.equal(30);
+    });
+  });
 });
