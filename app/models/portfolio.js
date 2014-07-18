3291414e2a778e752c009447b35e3e2c781c7a35
Logan Richardson <g.logan.richardson@gmail.com>
2014-07-18 05:39:23 -0500

    added del function

--- app/models/portfolio.js
+++ app/models/portfolio.js
@@ -1,22 +1,45 @@
 'use strict';
+var Stock = require('./stock');
 
 function Portfolio(name){
   this.name = name;
   this.stocks = [];
 }
 
-Portfolio.prototype.add = function(stock){
-  var add = true;
-  for(var i = 0; i < this.stocks.length; i++){
-    if(stock.symbol === this.stocks[i].symbol){
-      this.stocks[i].count += stock.count;
-      add = false;
-    }
-  }
 
-  if(add === true){
+Portfolio.prototype.add = function(symbol, amount){
+  var index = findStock(this.stocks, symbol);
+
+  if(index >= 0){
+    this.stocks[index].count += amount;
+  }else{
+    var stock = new Stock(symbol, amount);
     this.stocks.push(stock);
   }
 };
+
+Portfolio.prototype.del = function(symbol, amount){
+  var index = findStock(this.stocks, symbol);
+
+  if(index >= 0){
+    this.stocks[index].count -= amount;
+
+    if(this.stocks[index].count <= 0){
+      this.stocks.splice(index, 1);
+    }
+  }
+};
+
+//Private Helper Functions//
+
+
+function findStock(stocks, symbol){
+  for(var i = 0; i < stocks.length; i++){
+    if(stocks[i].symbol === symbol.toUpperCase()){
+      return i;
+    }
+  }
+  return -1;
+}
 
 module.exports = Portfolio;
