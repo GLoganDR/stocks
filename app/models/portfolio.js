fd6abd717fd0b6a433393b7f8b3680efa0315d9d
Brian Hiatt <brian.hiatt41@gmail.com>
2014-07-18 04:23:32 -0500

    added stock array

--- app/models/portfolio.js
+++ app/models/portfolio.js
@@ -1,11 +1,24 @@
 'use strict';
 
-var request = require('request');
+//var request = require('request');
 
 function Portfolio(name){
   this.name = name;
   this.stocks = [];
 }
 
+Portfolio.prototype.add = function(stock){
+  var add = true;
+  for(var i = 0; i < this.stocks.length; i++){
+    if(stock.symbol === this.stocks[i].symbol){
+      this.stocks[i].count += stock.count;
+      add = false;
+    }
+  }
+
+  if(add === true){
+    this.stocks.push(stock);
+  }
+};
 
 module.exports = Portfolio;
